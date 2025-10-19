b613a2679200b73bbeaab68a834049939ae2cb9a
chi1180 <cwchi1@outlook.jp>
2025-10-19 11:13:04 +0900

    fix: now we can import type of form

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,9 @@
 import { ParserEngine } from "./parseEngine";
 
+// Re-export all types
+export * from "./types/form.types";
+export * from "./types/json.types";
+
 export async function parseMarkdownToForm(markdown: string) {
   const _Engine = new ParserEngine(markdown);
   await _Engine.parse();
